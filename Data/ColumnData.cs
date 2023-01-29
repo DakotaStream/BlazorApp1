54f01a8b277782f5fef2cec6ba06dbfb9d071ab9
Michael Obermeyer <mike@dakotastream.com>
2023-01-28 19:43:41 -0600

    Added async table column gen.

--- Data/ColumnData.cs
+++ Data/ColumnData.cs
@@ -6,9 +6,7 @@ namespace BlazorApp1.Data
 		public int NumberFound { get; set; }
 		public DateOnly Date { get; set; }
 
-		public int TemperatureC { get; set; }
-
-		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+		public string? Icon { get; set; }
 
 		public string? Summary { get; set; }
 	}
--- Data/GeneratedColumns.cs
+++ Data/GeneratedColumns.cs
@@ -2,17 +2,27 @@ namespace BlazorApp1.Data
 {
 	public class GeneratedColumnsService
 	{
+		private static readonly string[] HeaderList = new[]
+		{
+		"Off", "Vacation", "Training", "Over Time", "Lunch", "Sick",
+			"PTO", "Holiday", "AWAL", "Remote", "Bereavemnt","Car Accident",
+			"Family Illness", " Family Emergency"
+		};
 		private static readonly string[] Summaries = new[]
 		{
 		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	};
+		};
+
+
+
 
 		public Task<ColumnData[]> GetColumnsAsync(DateOnly startDate)
 		{
-			return Task.FromResult(Enumerable.Range(1, 5).Select(index => new ColumnData
+			return Task.FromResult(Enumerable.Range(1, 13).Select(index => new ColumnData
 			{
+				HeaderName = HeaderList[index],
+				NumberFound = Random.Shared.Next(1, 30),
 				Date = startDate.AddDays(index),
-				TemperatureC = Random.Shared.Next(-20, 55),
 				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
 			}).ToArray());
 		}
